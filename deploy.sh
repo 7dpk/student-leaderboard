480e865e4464f38a328b0c6265275db0b3808ac3
bhanupratapkumar <78639550+bhanupratapkumar@users.noreply.github.com>
2021-05-18 15:55:18 +0530

    Tweaked deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ DB_NAME=test
 gcloud builds submit --tag gcr.io/$GOOGLE_PROJECT_ID/$CLOUD_RUN_SERVICE \
     --project=$GOOGLE_PROJECT_ID
 
-gcloud run deploy $ CLOUD_RUN_SERVICE \
+gcloud run deploy $CLOUD_RUN_SERVICE \
     --image gcr.io/$GOOGLE_PROJECT_ID/$CLOUD_RUN_SERVICE \
     --add-cloudsql-instances $INSTANCE_CONNECTION_NAME \
     --update-env-vars INSTANCE_CONNECTION_NAME=$INSTANCE_CONNECTION_NAME,DB_PASS=$DB_PASS,DB_USER=$DB_USER,DB_NAME=$DB_NAME \
